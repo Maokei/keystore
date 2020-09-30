7dc9e461aa26cdaaf39cbc05f95437bb1e01a80e
maokei <rickard.joh@gmail.com>
2020-09-30 12:47:11 +0200

    Verticle to for clustering

---
+++ src/main/java/se/maokei/keyvalue_store/LaunchVerticle.java
@@ -0,0 +1,9 @@
+package se.maokei.keyvalue_store;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+
+public class LaunchVerticle extends AbstractVerticle {
+  private static final Logger LOGGER = LoggerFactory.getLogger(LaunchVerticle.class);
+}
